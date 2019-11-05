2403951f988d3a19c4628b871d78726cfe2f9209
Ally Morson <ally.morson@gmail.com>
2019-11-05 00:08:31 +0000

    Done.

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -1,6 +1,5 @@
-module MetaDancing 
-  
+module MetaDancing
   def metadata
-       "This class produces objects that love to dance."
+    "This class produces objects that love to dance."
   end
 end
